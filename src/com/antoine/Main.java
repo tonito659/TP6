e87f2fd0caa760a487a165ef28a7daeb20641f9d
Antoine <fallenhaswon@gmail.com>
2016-11-18 11:21:46 +0100

    initial commit

---
+++ src/com/antoine/Main.java
@@ -0,0 +1,20 @@
+package com.antoine;
+
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static boolean palindrome(String mot){
+        // cette fonction détermine si le mot en entrée esst un palindrome !
+        for (int i=0; i<=mot.length(); i++){
+
+        }
+        return false;
+    }
+}
