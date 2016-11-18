d1ed6638899ad02947cd0fc4a2a3b676bc896588
Antoine <fallenhaswon@gmail.com>
2016-11-18 11:40:04 +0100

    deuxieme question done

--- src/com/antoine/Main.java
+++ src/com/antoine/Main.java
@@ -2,22 +2,35 @@ package com.antoine;
 
 import java.util.Scanner;
 
+
 public class Main {
 
-    Scanner sc = new Scanner(System.in);
+
 
     public static void main(String[] args) {
 	// write your code here
+        palindrome();
     }
 
-    public static boolean palindrome(String mot){
+    public static boolean isPalindrome(String phrase){
         // cette fonction détermine si le mot en entrée esst un palindrome !
         boolean pal = true;
-        for (int i=0; i<=mot.length(); i++){
-            if (mot.charAt(i)!= mot.charAt(mot.length()-i)){
-                pal = false // faut vérifier deux fois on sait jamais...
+        for (int i=0; i < phrase.length()-1; i++){
+            if (phrase.charAt(i)!= phrase.charAt(phrase.length()-i-1)){
+                pal = false;
             }
         }
         return pal;
+    }
+    public static void palindrome(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez une phrase: ");
+        String phrase = sc.nextLine();
+
+        if (isPalindrome(phrase))
+            {System.out.print("c'est un palindrome");
+        }else{
+            System.out.print("ce n'est pas un palindrome");
+        }
     }
 }
