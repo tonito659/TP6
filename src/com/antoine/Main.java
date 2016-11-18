0cef39ee54e37c6f88c55c2a16c6f2e509b2b547
Antoine <fallenhaswon@gmail.com>
2016-11-18 11:24:07 +0100

    Premiere question done

--- src/com/antoine/Main.java
+++ src/com/antoine/Main.java
@@ -12,9 +12,12 @@ public class Main {
 
     public static boolean palindrome(String mot){
         // cette fonction détermine si le mot en entrée esst un palindrome !
+        boolean pal = true;
         for (int i=0; i<=mot.length(); i++){
-
+            if (mot.charAt(i)!= mot.charAt(mot.length()-i)){
+                pal = false // faut vérifier deux fois on sait jamais...
+            }
         }
-        return false;
+        return pal;
     }
 }
