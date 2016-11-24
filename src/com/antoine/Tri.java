63e74a680f45740d25bcdffe18c7b6bedcdd59e0
Antoine <fallenhaswon@gmail.com>
2016-11-24 12:55:50 +0100

    Commencement question tri

--- src/com/antoine/Tri.java
+++ src/com/antoine/Tri.java
@@ -1,8 +1,21 @@
 package com.antoine;
-
+import java.util.Random;
 
 
 class Tri {
 
+
+    static Random rand = new Random();
+
+
+    public static int partitionner(int premier, int dernier){
+        int aleatoire = rand.nextInt((dernier - premier)+ 1) + premier;
+        return aleatoire;
+    }
+
+
+    public static void triRapide( int[] tableau){
+
+    }
 }
 
